e08b108ba07a0cdaab6d4e08048a68d0b0152c45
Gamzat <unichtojenmusor@gmail.com>
2021-03-30 02:11:56 +0300

    add UUID generic generator

--- src/main/java/com/xaghoul/demo/model/MessageTemplate.java
+++ src/main/java/com/xaghoul/demo/model/MessageTemplate.java
@@ -1,5 +1,6 @@
 package com.xaghoul.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,13 @@ import java.util.UUID;
 @Entity
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class MessageTemplate {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(generator = "UUID",
+                    strategy = GenerationType.IDENTITY)
     UUID id;
     String name;
     String substitutionValue;
