bc01f2cf596e99882297e65b5ca0408db6a69f8d
Gamzat <unichtojenmusor@gmail.com>
2021-03-30 02:11:15 +0300

    chore: add db properties & preloading message templates

--- src/main/java/com/xaghoul/demo/DemoApplication.java
+++ src/main/java/com/xaghoul/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.xaghoul.demo;
 
+import com.xaghoul.demo.model.MessageTemplate;
+import com.xaghoul.demo.repository.MessageTemplateRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,17 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner initDatabase(MessageTemplateRepository repository) {
+        return args -> {
+            repository.save(MessageTemplate.builder()
+                    .name("template1")
+                    .substitutionValue("A-Team")
+                    .build());
+            repository.save(MessageTemplate.builder()
+                    .name("template2")
+                    .build());
+
+        };
+    }
 }
