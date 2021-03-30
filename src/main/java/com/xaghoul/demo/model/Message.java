afc9b0f7e391e2ef47bcfb465855dd30d1ab2459
Gamzat <unichtojenmusor@gmail.com>
2021-03-31 00:23:28 +0300

    fix: proper storaging variables in Message

--- src/main/java/com/xaghoul/demo/model/Message.java
+++ src/main/java/com/xaghoul/demo/model/Message.java
@@ -1,11 +1,20 @@
 package com.xaghoul.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
-import java.util.List;
+import java.util.Map;
 
 @Value
 public class Message {
     String templateName;
-    List<String> variables;
+    Map<String, String> variables;
+
+    @JsonCreator
+    public Message(@JsonProperty String templateName,
+                   @JsonProperty Map<String, String> variables) {
+        this.templateName = templateName;
+        this.variables = variables;
+    }
 }
