ea5106d7cb55a184a920188327a0a12132e8eb76
Gamzat <unichtojenmusor@gmail.com>
2021-04-07 23:40:55 +0300

    add RestExceptionHandler

---
+++ src/main/java/com/xaghoul/demo/exception/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.xaghoul.demo.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler({MessageTemplateNotFoundException.class})
+    public ResponseEntity<Object> handleNotFound(MessageTemplateNotFoundException ex, WebRequest request) {
+        return handleExceptionInternal(ex, "Message template not found",
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
