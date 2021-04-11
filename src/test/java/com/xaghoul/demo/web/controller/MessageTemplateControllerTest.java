1697ef061b13aff4f64efda1087c5bc22d73ac90
Gamzat <unichtojenmusor@gmail.com>
2021-04-11 23:59:59 +0300

    add test

--- src/test/java/com/xaghoul/demo/web/controller/MessageTemplateControllerTest.java
+++ src/test/java/com/xaghoul/demo/web/controller/MessageTemplateControllerTest.java
@@ -1,22 +1,26 @@
 package com.xaghoul.demo.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xaghoul.demo.model.MessageTemplate;
+import com.xaghoul.demo.service.impl.MessageTemplateModelAssembler;
+import com.xaghoul.demo.service.impl.ScheduledMessageModelAssembler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.UUID;
 
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,13 +28,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MessageTemplateControllerTest {
 
     @Autowired
+    MessageTemplateModelAssembler templateModelAssembler;
+    @Autowired
+    ScheduledMessageModelAssembler messageModelAssembler;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private MessageTemplateController controller;
-
     @Test
-    void getById() throws Exception {
+    void getByIdTest() throws Exception {
         MessageTemplate template1 = new MessageTemplate(
                 UUID.randomUUID(), "template1", "Template $test$ message",
                 Arrays.asList(new URL("https://httpbin.org/post"),
@@ -48,7 +55,28 @@ class MessageTemplateControllerTest {
     }
 
     @Test
-    void getAllTemplates() {
+    void getAllTemplatesTest() throws Exception {
+        MessageTemplate template1 = new MessageTemplate(
+                UUID.randomUUID(), "template1", "Template $test$ message",
+                Arrays.asList(new URL("https://httpbin.org/post"),
+                        new URL("https://postman-echo.com/post")));
+        MessageTemplate template2 = new MessageTemplate(
+                UUID.randomUUID(), "$$$", "Today is $year$ year",
+                Collections.singletonList(new URL("https://httpbin.org/post")));
+
+        mockMvc.perform(post("/template/add_template")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(template1)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/template/add_template")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(template2)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/template/"))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
     @Test
@@ -56,22 +84,22 @@ class MessageTemplateControllerTest {
     }
 
     @Test
-    void addTemplate() {
+    void addTemplateTest() {
     }
 
     @Test
-    void sendMessage() {
+    void sendMessageTest() {
     }
 
     @Test
-    void cancelSendingMessage() {
+    void cancelSendingMessageTest() {
     }
 
     @Test
-    void updateTemplate() {
+    void updateTemplateTest() {
     }
 
     @Test
-    void deleteTemplate() {
+    void deleteTemplateTest() {
     }
 }
